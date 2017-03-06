706d09e9ec3c0a197aea26cf1419912c9cbf7e30
Samanta de Barros <samanta@wyeworks.com>
2017-03-05 22:30:23 -0300

    Add service worker

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,16 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     dotEnv: {
       clientAllowedKeys: ['API_KEY', 'USER_TOKEN']
+    },
+    'esw-cache-fallback': {
+      // RegExp patterns specifying which URLs to cache.
+      patterns: [
+        '*',
+        'https://api.trello.com/1/(.+)'
+      ],
+
+      // changing this version number will bust the cache
+      version: '1'
     }
   });
 
